a02e8344beeb6f3066af0ff8ec789a782edf9b24
RyanOD <odonnell.joseph@gmail.com>
2020-10-05 08:05:57 -0700

    Fixed parallax effect.

--- main.lua
+++ main.lua
@@ -6,10 +6,11 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
-BACKGROUND_SCROLL_SPEED = 130
+BACKGROUND_SCROLL_SPEED = 30
 BACKGROUND_LOOPING_POINT = 413
 
 GROUND_SCROLL_SPEED = 60
+GROUND_LOOPING_POINT = 560
 
 local background = love.graphics.newImage( 'images/background.png' )
 local backgroundScroll = 0
@@ -28,10 +29,10 @@ function love.load()
 end
 
 function love.update( dt )
-  backgroundScroll = backgroundScroll + dt * BACKGROUND_SCROLL_SPEED
-  if backgroundScroll > 413 then
-    backgroundScroll = 0
-  end
+  -- Use modulo here since when backgroundScroll equals 413, modulo will equal 0
+  backgroundScroll = ( backgroundScroll + BACKGROUND_SCROLL_SPEED * dt ) % BACKGROUND_LOOPING_POINT
+
+  groundScroll = ( groundScroll + GROUND_SCROLL_SPEED * dt ) % GROUND_LOOPING_POINT
 end
 
 function love.resize( w, h )
